e82168ac729b91bd7934db9c709827a43a5e8367
bartimus <admin@bartimus.site>
2020-06-10 13:49:08 -0400

    Initial

---
+++ Introduction/Introduction.py
@@ -0,0 +1,48 @@
+"""
+    Identifying the problem:
+    Get to work faster than walking.
+
+    #Identifying a solution:
+    Building a platform that can move a Person at a speed faster than 10 MPH.
+
+    Small pseudocode build out of the problem and the solution
+"""
+
+
+
+# Platform Components
+
+    # Chasis
+        # Body
+            # Door
+            # Hood
+        # Chair
+            # Seatbelt
+        # Instrument Panel
+            # Gas Gauge
+            # Speedometer
+                # Max Speed
+                # Min Speed
+    # DriveTrain
+        # Engine
+            # Fuel Type
+            # On/Off
+        # Transimission 
+            # Gear
+                # Park
+                # Drive
+        # Control
+            # Steering Wheel
+                # Horn
+                # Key
+            # Accelerator
+            # Brake
+
+# We Will set our project up, in a similar way to this layout.
+
+# Test Cases (A little talk about TDD, Test Driven Development -> Used often in DevOps)
+
+    # The car starts at point A and gets to Point B
+    # The car does not exceed the limit
+    # The car stops when the brake is pressed
+    # The car moves when the accelerator is pressed
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # programmingPrimerClass
 Some examples to go along with the programming primer course
+
+
+# This course has a powerpoint slide show to go with it.
